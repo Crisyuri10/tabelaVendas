fec3d357b142025af0feb2a883d727d3633dacfd
Crisyuri10 <crisyurisantos10@gmail.com>
2025-08-19 16:49:22 -0300

    Add files via upload
    teste

--- tabela.py
+++ tabela.py
@@ -1,16 +1,22 @@
 import streamlit as st
 import pandas as pd
+import os
+import pygsheets
 
-# ------------------------- CONFIGURAÇÃO -------------------------
 sheet_url = "https://docs.google.com/spreadsheets/d/1wqAJBWdS3jncP455KcJ0qpbJgzhVplOVG-JM4u8MvJU/edit#gid=0"
 aba_nome = "Página1"
 
-# ------------------------- CONEXÃO GOOGLE SHEETS -------------------------
-conn = st.experimental_connection("gsheets", type="gsheets")
+# ------------------------- DETECTA AMBIENTE -------------------------
+if "CLOUD_ENV" in os.environ:  # flag fictícia para Streamlit Cloud
+    # ------------------------- STREAMLIT CLOUD -------------------------
+    conn = st.experimental_connection("gsheets", type="gsheets")
+    df = conn.read(spreadsheet=sheet_url, worksheet=aba_nome)
+else:
+    # ------------------------- LOCAL -------------------------
+    gc = pygsheets.authorize(service_file="cred.json")
+    aba = gc.open_by_url(sheet_url).worksheet_by_title(aba_nome)
+    df = pd.DataFrame(aba.get_all_records())
 
-# ------------------------- LEITURA DADOS -------------------------
-df = conn.read(spreadsheet=sheet_url, worksheet=aba_nome)
-
-# ------------------------- EXIBIR TABELA -------------------------
+# ------------------------- EXIBIÇÃO -------------------------
 st.title("📊 Dados da Página 1")
 st.dataframe(df, use_container_width=True)
