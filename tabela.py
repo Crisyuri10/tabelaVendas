89c53984a4b900be1921ff515aeda05587ef7e70
Crisyuri10 <crisyurisantos10@gmail.com>
2025-08-19 16:46:07 -0300

    Add files via upload
    testes

--- tabela.py
+++ tabela.py
@@ -2,16 +2,17 @@ import streamlit as st
 import pandas as pd
 import pygsheets
 
-# ----------------------- AUTENTICAÇÃO GOOGLE SHEETS -----------------------
-credenciais_dict = st.secrets["gcp_service_account"]  # pega o dict direto
+# Pega o dict direto do secrets
+credenciais_dict = st.secrets["gcp_service_account"]
+
+# Autenticação via service account
 gc = pygsheets.authorize(service_account_info=credenciais_dict)
 
 sheet_url = "https://docs.google.com/spreadsheets/d/1wqAJBWdS3jncP455KcJ0qpbJgzhVplOVG-JM4u8MvJU/edit#gid=0"
 
-# ----------------------- LEITURA DADOS -----------------------
+# Abrir planilha e aba
 aba = gc.open_by_url(sheet_url).worksheet_by_title("Página1")
 df = pd.DataFrame(aba.get_all_records())
 
-# ----------------------- EXIBIR TABELA -----------------------
 st.title("📊 Dados da Página 1")
 st.dataframe(df, use_container_width=True)
