545a1e2ec9b023ad70e4dbdb8a5e044f59bdd1ab
Crisyuri10 <crisyurisantos10@gmail.com>
2025-08-19 16:42:59 -0300

    Add files via upload
    teste

--- tabela.py
+++ tabela.py
@@ -1,13 +1,17 @@
 import streamlit as st
 import pandas as pd
 import pygsheets
+import json
 
 # ----------------------- AUTENTICAÇÃO GOOGLE SHEETS -----------------------
-credenciais_path = "cred.json"
-gc = pygsheets.authorize(service_file=credenciais_path)
-sheet_url = "https://docs.google.com/spreadsheets/d/1wqAJBWdS3jncP455KcJ0qpbJgzhVplOVG-JM4u8MvJU/edit?gid=0"
+credenciais_json = st.secrets["gcp_service_account"]
+credenciais_dict = json.loads(json.dumps(credenciais_json))  # Converte para dict
 
-# ----------------------- LEITURA DADOS PÁGINA 4 -----------------------
+gc = pygsheets.authorize(service_account_info=credenciais_dict)
+
+sheet_url = "https://docs.google.com/spreadsheets/d/1wqAJBWdS3jncP455KcJ0qpbJgzhVplOVG-JM4u8MvJU/edit#gid=0"
+
+# ----------------------- LEITURA DADOS PÁGINA1 -----------------------
 aba = gc.open_by_url(sheet_url).worksheet_by_title("Página1")
 df = pd.DataFrame(aba.get_all_records())
 
