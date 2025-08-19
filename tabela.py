2ac25c77792bc5a3299940251af06e6f2807c67b
Crisyuri10 <crisyurisantos10@gmail.com>
2025-08-19 15:52:13 -0300

    Add files via upload
    teste

--- tabela.py
+++ tabela.py
@@ -1,18 +1,9 @@
 import streamlit as st
 import pandas as pd
 import pygsheets
-import os
-import json
 
 # ----------------------- AUTENTICAÇÃO GOOGLE SHEETS -----------------------
-credenciais_path = "cred.json"
-
-if os.path.exists(credenciais_path):
-    # Ambiente local
-    gc = pygsheets.authorize(service_file=credenciais_path)
-else:
-    # Streamlit Cloud -> pega das secrets
-    gc = pygsheets.authorize(service_account_info=st.secrets["gcp_service_account"])
+gc = pygsheets.authorize(service_account_info=dict(st.secrets["gcp_service_account"]))
 
 sheet_url = "https://docs.google.com/spreadsheets/d/1wqAJBWdS3jncP455KcJ0qpbJgzhVplOVG-JM4u8MvJU/edit?gid=0"
 
