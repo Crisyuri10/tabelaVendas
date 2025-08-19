1960891071093307552e8feaa7dfe5293517e17a
Crisyuri10 <crisyurisantos10@gmail.com>
2025-08-19 17:36:01 -0300

    Add files via upload
    ss

--- tabela.py
+++ tabela.py
@@ -1,15 +1,19 @@
 import streamlit as st
 import pandas as pd
+import pygsheets
 
 sheet_url = "https://docs.google.com/spreadsheets/d/1wqAJBWdS3jncP455KcJ0qpbJgzhVplOVG-JM4u8MvJU/edit#gid=0"
 aba_nome = "Página1"
 
-# Conexão com Google Sheets via Streamlit Cloud
-conn = st.experimental_connection("gsheets", type="gsheets")
+# Pega as credenciais do Streamlit Cloud
+credenciais_dict = st.secrets["gcp_service_account"]
 
-# Lê os dados da aba
-df = conn.read(spreadsheet=sheet_url, worksheet=aba_nome)
+# Autoriza usando service account
+gc = pygsheets.authorize(service_account_info=credenciais_dict)
 
-# Exibição
+# Lê os dados
+aba = gc.open_by_url(sheet_url).worksheet_by_title(aba_nome)
+df = pd.DataFrame(aba.get_all_records())
+
 st.title("📊 Dados da Página 1")
 st.dataframe(df, use_container_width=True)
